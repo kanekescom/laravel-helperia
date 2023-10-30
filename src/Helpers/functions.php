14e09066efaca07fcb59dab49f37b3c2739b9d69
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2023-10-30 10:44:22 +0700

    Remove __ lead on get method class

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -13,10 +13,18 @@ if (! function_exists('method_public')) {
     function method_public($className): Collection
     {
         $reflection = new ReflectionClass($className);
-        $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+        $methodNames = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+
+        $methodNames = array_filter($methodNames, function ($method) {
+            return ! $method->isConstructor()
+                && ! $method->isDestructor()
+                && ! $method->isInternal()
+                && strpos($method->name, '__') !== 0;
+        });
+
         $methodNames = array_map(function ($method) {
             return $method->name;
-        }, $methods);
+        }, $methodNames);
 
         return collect($methodNames);
     }
@@ -26,10 +34,18 @@ if (! function_exists('method_protected')) {
     function method_protected($className): Collection
     {
         $reflection = new ReflectionClass($className);
-        $methods = $reflection->getMethods(ReflectionMethod::IS_PROTECTED);
+        $methodNames = $reflection->getMethods(ReflectionMethod::IS_PROTECTED);
+
+        $methodNames = array_filter($methodNames, function ($method) {
+            return ! $method->isConstructor()
+                && ! $method->isDestructor()
+                && ! $method->isInternal()
+                && strpos($method->name, '__') !== 0;
+        });
+
         $methodNames = array_map(function ($method) {
             return $method->name;
-        }, $methods);
+        }, $methodNames);
 
         return collect($methodNames);
     }
@@ -39,10 +55,18 @@ if (! function_exists('method_private')) {
     function method_private($className): Collection
     {
         $reflection = new ReflectionClass($className);
-        $methods = $reflection->getMethods(ReflectionMethod::IS_PRIVATE);
+        $methodNames = $reflection->getMethods(ReflectionMethod::IS_PRIVATE);
+
+        $methodNames = array_filter($methodNames, function ($method) {
+            return ! $method->isConstructor()
+                && ! $method->isDestructor()
+                && ! $method->isInternal()
+                && strpos($method->name, '__') !== 0;
+        });
+
         $methodNames = array_map(function ($method) {
             return $method->name;
-        }, $methods);
+        }, $methodNames);
 
         return collect($methodNames);
     }
